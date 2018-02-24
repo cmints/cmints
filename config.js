82b25b4296acb3fdeba8dd0a17fe373a54bd406f
Manvel Saroyan <saroyanm@gmail.com>
2018-02-24 23:23:02 +0100

    Generate less map

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ const srcPath = "./src";
 const pageDir = `${srcPath}/pages`;
 const assetsDir = `${srcPath}/assets`;
 
-
 exports.srcPath = srcPath;
 exports.pageDir = pageDir;
 exports.assetsDir = assetsDir;
