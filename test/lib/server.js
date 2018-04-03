efee0361bdbec4d414745de0590ce16337203f14
Manvel Saroyan <saroyanm@gmail.com>
2018-04-03 21:07:22 +0200

    #28 - Added the testcase

--- test/lib/server.js
+++ test/lib/server.js
@@ -11,7 +11,7 @@ const pathCodes = {
   200: ["", "test", "ru/test", "ru/test", "test/path1", "test/path1/subpath1",
         "test/main.css"],
   404: ["index", "test/index", "ru/test/index", "test/index.md", "test/path1.md",
-        "test/logo.png", "public/test/main.css" /*, Fixme "ru/test/path1" */]
+        "test/logo.png", "public/test/main.css", "de/test/path1"]
 };
 const caches = ["en/test/index.html", "ru/test/index.html", "en/test/path1.html",
                 "test/main.css"];
