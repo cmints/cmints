569c8fe64e410d03d45b6624ecaa295d4ba19586
Manvel Saroyan <saroyanm@gmail.com>
2018-05-31 18:53:37 +0200

    Consider the case that private module shouldn't exist for bundling browserify

--- test/lib/bundle.js
+++ test/lib/bundle.js
@@ -3,10 +3,13 @@ const fs = require("fs");
 const readFile = promisify(fs.readFile);
 const resultDir = "./test/results";
 const {publicDir} = require("../../config").dirs;
+const ensureFile = require("fs-extra").ensureFile;
 
 const files = [
-  [`${publicDir}/js/test/beep.js`, `${resultDir}/public/js/test/beep.js`],
-  [`${publicDir}/js/test/subFolder/boop.js`, `${resultDir}/public/js/test/subFolder/boop.js`]
+  [`${publicDir}/js/test/beep.js`,
+   `${resultDir}/public/js/test/beep.js`],
+  [`${publicDir}/js/test/subFolder/boop.js`,
+   `${resultDir}/public/js/test/subFolder/boop.js`]
 ]
 
 for (const file of files)
@@ -14,7 +17,17 @@ for (const file of files)
   compare(file[0], file[1]);
 }
 
+const privateModule = `${publicDir}/js/test/modules/_robot.js`;
 
+describe(`Check if private module ${privateModule} exists`, () =>
+{
+  it(`Private module shouldn't exist`, (done) =>
+  {
+    const isModuleExist = fs.existsSync(privateModule);
+    isModuleExist.should.be.false;
+    done();
+  });
+});
 
 function compare(sourceFile, resultFile)
 {
@@ -22,7 +35,9 @@ function compare(sourceFile, resultFile)
   {
     it(`Should be same`, (done) =>
     {
-      Promise.all([readFile(sourceFile, "utf-8"), readFile(resultFile, "utf-8")]).then(([sourceContent, resultContent]) =>
+      Promise.all([readFile(sourceFile, "utf-8"), 
+                   readFile(resultFile, "utf-8")]).then(([sourceContent,
+                                                          resultContent]) =>
       {
         let smth = sourceContent == resultContent;
         sourceContent.should.be.equal(resultContent)
