149de98c040ac6c53b79a8d4188e3797f2dbdb95
Manvel Saroyan <saroyanm@gmail.com>
2018-08-06 21:49:10 +0200

    Generate content using actual script and separate the pre-test from the static content generation test step

--- test/pre-test.js
+++ test/pre-test.js
@@ -1,17 +1,7 @@
-const {copySync, moveSync} = require("fs-extra");
+const {copySync, moveSync, ensureDirSync} = require("fs-extra");
 const {srcPath} = require("../config").dirs;
 const targetDir = "./test/src";
-const {prepareApp} = require("./bin/app");
-const {generateStatic} = require("../lib/server");
-const argv = require("minimist")(process.argv.slice(2));
 
+ensureDirSync(srcPath);
 moveSync(srcPath, `${srcPath}-tmp`);
 copySync(targetDir, srcPath);
-
-if (argv.static)
-{
-  prepareApp(() =>
-  {
-    generateStatic(process.exit);
-  });
-}
