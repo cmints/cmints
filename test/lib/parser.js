0a2a5d6cca15fda5bc4fda628595142c47e0a9b7
Manvel Saroyan <saroyanm@gmail.com>
2018-08-12 15:20:27 +0200

    #83 - Created tests for Helpers

--- test/lib/parser.js
+++ test/lib/parser.js
@@ -5,8 +5,15 @@ const {promisify} = require("util");
 const fs = require("fs");
 const readFile = promisify(fs.readFile);
 
-let parserArgs = ["index", ".md", "ru"];
-let resultFile = "index.html";
+let parserArgs = [];
+let resultFile = "";
+// Testing index.html
+parserArgs = ["index", ".md", "ru"];
+resultFile = "index.html";
+parserResult(parserArgs, `${resultDir}/${resultFile}`);
+// Testing helpers helpers/index.html
+parserArgs = ["helpers/index", ".ejs", "ru"];
+resultFile = "helpers/index.html";
 parserResult(parserArgs, `${resultDir}/${resultFile}`);
 
 function parserResult(parserArgs, resultPath)
