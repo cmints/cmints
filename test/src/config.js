2d14570bd18a471b678cd5c330ca5c053b88dd65
Manvel Saroyan <saroyanm@gmail.com>
2018-11-01 20:58:45 +0400

    #122 - Use Cache option for tests

--- test/src/config.js
+++ test/src/config.js
@@ -4,9 +4,9 @@ const argv = require("minimist")(process.argv.slice(2));
 
 if (argv.addgzip)
 {
-  module.exports = {gzip: true};
+  module.exports = {gzip: true, generationType: "Cache"};
 }
 else
 {
-  module.exports = {example: "http://localhost:3000/test/example-test.zip"};
+  module.exports = {example: "http://localhost:3000/test/example-test.zip", generationType: "Cache"};
 }
